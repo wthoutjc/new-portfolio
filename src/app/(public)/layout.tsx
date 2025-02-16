cdc31b9474cfc001e99ad62c6cf88c91298d0807
wthoutjc <juancamilorr2012@hotmail.com>
2025-02-16 11:41:53 -0500

    feat: navbar

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,24 @@
+import { AppNavbar } from "@/components/ui/app-navbar/app-navbar";
+
 export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col max-w-full min-h-screen">
+      <AppNavbar />
+      <div className="flex flex-[1_0_auto] justify-center">
+        <div className="flex flex-col max-w-7xl w-full md:flex-row">
+          <div>asd</div>
+          <div
+            className="flex flex-col p-3 md:pt-3 md:p-0 w-full md:w-3/4"
+            id="content"
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ui/app-navbar/app-navbar.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { ModeToggle } from "../toggle-theme/toggle-theme";
+
+const items: { title: string; href: string; description: string }[] = [
+  {
+    title: "Experiencia",
+    href: "/experience",
+    description: "Experiencia profesional",
+  },
+  {
+    title: "Educación",
+    href: "/education",
+    description: "Formación académica",
+  },
+  {
+    title: "Habilidades",
+    href: "/skills",
+    description: "Habilidades y tecnologías",
+  },
+];
+
+const AppNavbar = () => {
+  return (
+    <div className="flex max-w-full p-5 justify-center drop-shadow-sm h-[60px] relative">
+      <div className="flex max-w-7xl w-full justify-between items-center">
+        <div>
+          <li className="dark:flex w-24 opacity-80 hover:opacity-100 ease-out duration-300 hidden">
+            <Link href="/">
+              <Image
+                className="object-cover"
+                src="https://res.cloudinary.com/ddmeptk5c/image/upload/f_auto,q_auto/v1/portfolio/nrmlj5wmeop1rcqp5hdp"
+                alt="ionjc"
+                width={100}
+                height={100}
+              />
+            </Link>
+          </li>
+          <li className="flex w-24 opacity-80 hover:opacity-100 ease-out duration-300 dark:hidden">
+            <Link href="/">
+              <Image
+                className="object-cover"
+                src="https://res.cloudinary.com/ddmeptk5c/image/upload/f_auto,q_auto/v1/portfolio/wonlwuluroldfu03zcml"
+                alt="ionjc"
+                width={100}
+                height={100}
+              />
+            </Link>
+          </li>
+        </div>
+        <NavigationMenu className="justify-end">
+          <NavigationMenuList className="space-x-8 mr-4">
+            {items.map((item) => (
+              <NavigationMenuItem key={item.title}>
+                <Link href={item.href} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    {item.title}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
+
+export { AppNavbar };
